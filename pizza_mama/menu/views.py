7f12cb3cc51b3f69557422019e4c6c27e6cf22c1
Sleyter Monteiro <87656234+PrivacyNemesis@users.noreply.github.com>
2023-06-18 15:13:52 +0200

    sqlite et vue

--- pizza_mama/menu/views.py
+++ pizza_mama/menu/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from .models import Pizza
 # Create your views here.
+
+
+
+
+
+
+def index(request):
+    # pizzas = Pizza.objects.all()
+    # pizza_names_and_price = [pizza.nom + " Prix : " + str(pizza.prix) + "€" for pizza in pizzas]
+    # pizza_names_and_price_str = ", ".join(pizza_names_and_price)
+    # return HttpResponse("Les Pizzas : " + pizza_names_and_price_str)
+    return render(request, 'menu/index.html')
